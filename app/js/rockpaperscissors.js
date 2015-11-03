04f3812d8437a3ad71127ff94d26a7d9940096d4
Rhiannon Argote <rhiannonland@Rhiannons-MacBook-Pro.local>
2015-11-03 22:46:10 +1100

    Added Player Move

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,10 +22,11 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    if (move == null) {
+        return getInput();
+    } else {
+        return move;
+    }
 }
 
 function getComputerMove(move) {
