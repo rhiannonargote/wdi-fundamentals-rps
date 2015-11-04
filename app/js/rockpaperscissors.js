b48fa47f1e263a052e6db9987cba12228bfda5a6
Rhiannon Argote <rhiannon.argote@gmail.com>
2015-11-05 00:14:46 +1100

    Run rock paper scissors game until computer or player has won five times

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -60,6 +60,21 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
+
+    while (playerWins < 5 || computerWins < 5) {
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        var winner = getWinner(playerMove,computerMove);
+        if (winner === 'player') {
+            playerWins += 1;
+        } else if ( winner === 'computer') {
+            computerWins += 1;
+        } else (playerMove === computerMove) 
+            console.log("It's a tie!")
+        }
+        console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+        console.log('The score is currently ' + playerWins + ' to ' + computerWins);
+    
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
     return [playerWins, computerWins];
